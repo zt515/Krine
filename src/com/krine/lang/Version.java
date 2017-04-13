938c96b668e0539846e04f26cbc27c807d3e0f91
zt515 <kiva515@foxmail.com>
2017-04-14 00:09:54 +0800

    Release: Krine 2.2 (Blue)

--- src/com/krine/lang/Version.java
+++ src/com/krine/lang/Version.java
@@ -1,11 +1,14 @@
 package com.krine.lang;
 
+@SuppressWarnings("unused")
 public final class Version
 {
-    public static final String KRINE_2_0 = "Krine 2.0 (Alpha)";
+    public static final String KRINE_1_0 = "Krine 1.0 (original Dragon)";
+    public static final String KRINE_2_0 = "Krine 2.0 (Asteroid)";
     public static final String KRINE_2_1 = "Krine 2.1 (Asteroid)";
+    public static final String KRINE_2_2 = "Krine 2.2 (Blue)";
     
     public static String current() {
-        return KRINE_2_1;
+        return KRINE_2_2;
     }
 }
